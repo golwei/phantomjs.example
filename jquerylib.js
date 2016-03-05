6d9464dce141151f149cf603e8e9e8809c0c8d65
golwei <lizhiweimail@qq.com>
2016-03-05 15:01:31 +0800

    Update and rename jquerylib to jquerylib.js

---
+++ jquerylib.js
@@ -0,0 +1,32 @@
+var page = require('webpage').create();
+page.open('http://www.baidu.com', function() {
+  page.includeJs("", function() {
+    page.evaluate(function() {
+      $("button").click();
+    });
+    phantom.exit()
+  });
+});
+
+
+// Read the Phantom webpage '#intro' element text using jQuery and "includeJs"
+
+"use strict";
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+page.open("http://phantomjs.org/", function(status) {
+    if (status === "success") {
+        page.includeJs("http://cdn.bootcss.com/jquery/1.6.4/jquery.min.js", function() {
+            page.evaluate(function() {
+                console.log("$(\".explanation\").text() -> " + $(".explanation").text());
+            });
+            phantom.exit(0);
+        });
+    } else {
+      phantom.exit(1);
+    }
+});
